2f635a6f13cce2a5674d80da8e057effaf890555
zicoovic <abdelrhmanzakariaqa@gmail.com>
2024-08-15 15:41:36 +0300

    -added Development and production flavor for android -added launch.json for flavour config -changed android app name for each flavor -added two main files for each flavor

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -2,9 +2,12 @@ import 'package:doctor_appointment_app/core/di/dependency_injection.dart';
 import 'package:doctor_appointment_app/core/routing/app_router.dart';
 import 'package:doctor_appointment_app/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
   setupGetIt();
+   // to fix Text being hidden bug in flutter_screenutil in release mode
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
